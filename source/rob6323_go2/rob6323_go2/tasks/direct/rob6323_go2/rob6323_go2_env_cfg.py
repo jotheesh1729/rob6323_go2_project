fab522bbfda095170d421baa3dd965518e301a71
vedvyas danturi <vsd2027@nyu.edu>
2025-12-19 10:01:56 -0500

    changing reward scales for lesser gradient varience

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -131,10 +131,10 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     #part --5 unnatural walking penalties
     orient_reward_scale = -5.0
     lin_vel_z_reward_scale = -0.02
-    dof_vel_reward_scale = -0.0001
+    dof_vel_reward_scale = -0.001 #increased penalty for fast joint motion
     ang_vel_xy_reward_scale = -0.001
     #part --6 contact forces and foot clearance
-    feet_clearance_reward_scale = -40.0 # changed by 10 to check performance
+    feet_clearance_reward_scale = -20.0 # changed by 10 to check performance
     tracking_contacts_shaped_force_reward_scale = 4.0
     #torque magnitude penalty
     torque_reward_scale = -0.0001
