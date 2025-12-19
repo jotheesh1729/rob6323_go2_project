3fbc3ad294acc7572d6fd7c9432622a2ea17a799
vedvyas danturi <vsd2027@nyu.edu>
2025-12-19 08:19:10 -0500

    added randomized robot body friction for interaction with environment

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -18,6 +18,25 @@ from isaaclab.sensors import ContactSensorCfg
 from isaaclab.markers import VisualizationMarkersCfg
 from isaaclab.markers.config import BLUE_ARROW_X_MARKER_CFG, FRAME_MARKER_CFG, GREEN_ARROW_X_MARKER_CFG
 
+from isaaclab.managers import EventTermCfg as EventTerm #adding for friction randomization
+from isaaclab.managers import SceneEntityCfg #for finding all bodies with name robot
+
+# took from documentation https://isaac-sim.github.io/IsaacLab/main/source/tutorials/03_envs/create_direct_rl_env.html
+@configclass
+class EventCfg:
+  robot_physics_material = EventTerm(
+      func=mdp.randomize_rigid_body_material,
+      mode="reset",
+      params={
+          "asset_cfg": SceneEntityCfg("robot", body_names=".*"), #getting all robot bodies
+          "static_friction_range": (0.7, 1.0), 
+          "dynamic_friction_range": (0.6, 1.0),
+          "restitution_range": (1.0, 1.0),
+          "num_buckets": 250,
+          "make_consistent": True #checked all params to add https://isaac-sim.github.io/IsaacLab/main/_modules/isaaclab/envs/mdp/events.html#randomize_rigid_body_material
+      },
+  )
+
 @configclass
 class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     # env
