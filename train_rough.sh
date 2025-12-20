3adbc28724b721559eb1bb2847a3b2a6b373bede
John Abanes <jya9538@nyu.edu>
2025-12-19 21:52:42 -0500

    Add train_rough.sh script for job submission

---
+++ train_rough.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+ssh -o StrictHostKeyChecking=accept-new burst "cd ~/rob6323_go2_project && sbatch --job-name='rob6323_rough_${USER}' --mail-user='${USER}@nyu.edu' train_rough.slurm '$@'"
